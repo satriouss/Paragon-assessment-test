16c67d39b1adf4df7626aa80d49fd6f10ce22bea
satriouss <38514097+satriouss@users.noreply.github.com>
2025-09-08 14:07:19 +0700

    Add files via upload

---
+++ cypress-prgn/cypress/e2e/register.cy.js
@@ -0,0 +1,13 @@
+describe('Register Scenarios', () => {
+  const baseUrl = "https://recruitment-staging-queenbee.paradev.io";
+
+  cy.get('body').click(100, 200);
+
+  cy.contains("Daftar").click();
+
+  cy.get('input[name="fullname"]').type("Akun Contoh");
+  cy.get('input[name="phone"]').type("081234567890");
+  cy.get('input[name="email"]').type("akuncontoh" + Date.now() + "@gmail.com");
+  cy.get('input[name="password"]').type("Password123!");
+  cy.contains("Daftar").click();
+});
